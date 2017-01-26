a2809da2b03889fee8722827631d787f31ad1f08
Chathura Hettiarachchi <Chathura.h@fidenz.com>
2017-01-26 14:10:43 +0530

    Initial commit

---
+++ mobile/src/main/java/com/chootdev/sample/SampleMobileActivity.java
@@ -0,0 +1,13 @@
+package com.chootdev.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SampleMobileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_mobile);
+    }
+}
---
+++ typefaced/src/main/java/com/chootdev/typefaced/TypeFacedButton.java
@@ -0,0 +1,8 @@
+package com.chootdev.typefaced;
+
+/**
+ * Created by Choota on 1/26/17.
+ */
+
+public class TypeFacedButton {
+}
